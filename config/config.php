9c93d14e03a5697abc08e78fdb8ba47f8bc92b92
starite paschal <paschalnwokeocha@gmail.com>
2022-04-24 19:29:57 +0100

    ok

--- config/config.php
+++ config/config.php
@@ -8,4 +8,12 @@ $connection = mysqli_connect($hostname, $username , $password, $database);
 $base_url =  "https://kykadesigns.com/";
 $my_email = "INFO@KYKADESIGNS.com";
 
+// session_start();
+// $hostname = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "crypto";
+// $connection = mysqli_connect($hostname, $username , $password, $database);
+// $base_url =  "https://kykadesigns.com/";
+// $my_email = "INFO@KYKADESIGNS.com";
 ?>
