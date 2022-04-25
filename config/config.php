931db01fcb693316a00120f596de1f9646e04c89
starite paschal <paschalnwokeocha@gmail.com>
2022-04-25 09:43:38 +0100

    gold

--- config/config.php
+++ config/config.php
@@ -1,9 +1,18 @@
 <?php
+// session_start();
+// $hostname = "localhost";
+// $username = "kykagrez_starite";
+// $password = "Kykadesign@090";
+// $database = "kykagrez_crypto";
+// $connection = mysqli_connect($hostname, $username , $password, $database);
+// $base_url =  "https://kykadesigns.com/";
+// $my_email = "INFO@KYKADESIGNS.com";
+
 session_start();
 $hostname = "localhost";
-$username = "kykagrez_starite";
-$password = "Kykadesign@090";
-$database = "kykagrez_crypto";
+$username = "kykagrez_pope";
+$password = "paschal@081";
+$database = "kykagrez_pope";
 $connection = mysqli_connect($hostname, $username , $password, $database);
 $base_url =  "https://kykadesigns.com/";
 $my_email = "INFO@KYKADESIGNS.com";
